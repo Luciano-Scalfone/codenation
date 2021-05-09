a5d35ee3d4670c596829f7e9640a3e2053939010
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-05-09 01:41:24 -0300

    desafio 1

--- java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -1,15 +1,39 @@
 package br.com.codenation.desafioexe;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class DesafioApplication {
+	static List<Integer> fibonacciList = new ArrayList<>();
+	static int number = 0;
 
 	public static List<Integer> fibonacci() {
-		return null;
+		fibonacciList.add(0);
+		fibonacciList.add(1);
+
+		while(number < 350) {
+			number = fibonacciList.get(fibonacciList.size() - 1) + fibonacciList.get(fibonacciList.size() - 2);
+			fibonacciList.add(number);
+		};
+
+		System.out.println(fibonacciList);
+
+		return fibonacciList;
 	}
 
 	public static Boolean isFibonacci(Integer a) {
-		return null;
-	}
+		fibonacciList.add(0);
+		fibonacciList.add(1);
 
+		while(number <= a) {
+			number = fibonacciList.get(fibonacciList.size() - 1) + fibonacciList.get(fibonacciList.size() - 2);
+			fibonacciList.add(number);
+		};
+
+		if(fibonacciList.indexOf(a) >= 0) {
+			return true;
+		};
+
+		return false;
+	}
 }
